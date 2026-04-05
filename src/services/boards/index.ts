e61f5a14569a8f6393059482af4ce0531789cedb
Jaime Sebastian Chavarria Fuertes <jschavarria77@gmail.com>
2026-04-04 23:58:02 -0400

    feat: add index.ts in services/boards

---
+++ src/services/boards/index.ts
@@ -0,0 +1,4 @@
+export { createBoardService } from "@/src/services/boards/create";
+export { getAllByWorkspaceService } from "@/src/services/boards/get-all";
+export { replaceBoardService } from "@/src/services/boards/put";
+export { deleteBoardService } from "@/src/services/boards/delete";
