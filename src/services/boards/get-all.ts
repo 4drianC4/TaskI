a60c141239613ddbc60f89665b1ea1e0a1cb8b2d
Jaime Sebastian Chavarria Fuertes <jschavarria77@gmail.com>
2026-04-04 23:17:25 -0400

    feat: add get all workspace boards service

---
+++ src/services/boards/get-all.ts
@@ -0,0 +1,14 @@
+import { prisma } from "@/lib/prisma";
+import type { BoardDTO } from "@/types/boards";
+import { toBoardDTO } from "@/src/services/boards/service";
+
+export async function getAllByWorkspaceService(
+  workspaceId: string,
+): Promise<BoardDTO[]> {
+  const normalizedWorkspaceId = workspaceId.trim();
+  const allBoards = await prisma.boards.findMany({
+    where: { workspace_id: normalizedWorkspaceId, state: "active" },//duda aqui xd
+    orderBy: { created_at: "desc" }
+  });
+  return allBoards.map(toBoardDTO);
+}
