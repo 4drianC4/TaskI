246c4ad7ab2591e9555f8f30da833710f511b996
Mijael Luján <lujanmijael@gmail.com>
2026-05-14 16:44:27 -0400

    feat: add column in collection and correct id generation

--- src/services/column/create.ts
+++ src/services/column/create.ts
@@ -1,10 +1,12 @@
 import { prisma } from "@/lib/prisma";
 import { toColumnDTO } from "@/src/services/column/service";
 import { ColumnDTO, CreateColumnInput } from "@/types/column";
+import { randomUUID } from "crypto";
 
 export async function createColumnService(input: CreateColumnInput): Promise<ColumnDTO> {
   const column = await prisma.columns.create({
     data: {
+      id: randomUUID(),
       board_id: input.board_id,
       name: input.name,
       order: input.order,
